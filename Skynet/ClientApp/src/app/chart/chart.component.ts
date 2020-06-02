e7aa86e0c81cbc37b492cd621e85f41542ceb386
kristian Vincentzen <kvincentzen@gmail.com>
2020-06-02 10:25:31 +0200

    ngx-chart

--- Skynet/ClientApp/src/app/chart/chart.component.ts
+++ Skynet/ClientApp/src/app/chart/chart.component.ts
@@ -1,35 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { multi } from './data';
 
 @Component({
-    selector: 'line-chart',
+    selector: 'app-chart',
     templateUrl: './chart.component.html',
+    styleUrls: ['./chart.component.css']
 })
 export class ChartComponent {
-    public chartType: string = 'line';
+    multi: any[];
+    view: any[] = [700, 300];
 
-    public chartDatasets: Array<any> = [
-        { data: [65, 59, 80, 81, 56, 55, 40], label: 'My First dataset' },
-        { data: [28, 48, 40, 19, 86, 27, 90], label: 'My Second dataset' }
-    ];
+    // options
+    legend: boolean = true;
+    showLabels: boolean = true;
+    animations: boolean = true;
+    xAxis: boolean = true;
+    yAxis: boolean = true;
+    showYAxisLabel: boolean = true;
+    showXAxisLabel: boolean = true;
+    xAxisLabel: string = 'Year';
+    yAxisLabel: string = 'Population';
+    timeline: boolean = true;
 
-    public chartLabels: Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-
-    public chartColors: Array<any> = [
-        {
-            backgroundColor: 'rgba(105, 0, 132, .2)',
-            borderColor: 'rgba(200, 99, 132, .7)',
-            borderWidth: 2,
-        },
-        {
-            backgroundColor: 'rgba(0, 137, 132, .2)',
-            borderColor: 'rgba(0, 10, 130, .7)',
-            borderWidth: 2,
-        }
-    ];
-
-    public chartOptions: any = {
-        responsive: true
+    colorScheme = {
+        domain: ['#5AA454', '#E44D25', '#CFC0BB', '#7aa3e5', '#a8385d', '#aae3f5']
     };
-    public chartClicked(e: any): void { }
-    public chartHovered(e: any): void { }
+
+    constructor() {
+        Object.assign(this, { multi });
+    }
+
+    onSelect(data): void {
+        console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+    }
+
+    onActivate(data): void {
+        console.log('Activate', JSON.parse(JSON.stringify(data)));
+    }
+
+    onDeactivate(data): void {
+        console.log('Deactivate', JSON.parse(JSON.stringify(data)));
+    }
 }
