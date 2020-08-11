0a5c93dcc26e9a8a5c50c861a6389bda2d7e2c78
Jakob Vestergaard <jakobmetalv@gmail.com>
2020-08-11 08:23:13 +0200

    Reset

---
+++ Envir/Envir/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Envir.Models
+{
+    public class User
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Role { get; set; }
+        public string Location { get; set; }
+        public string Username { get; set; }
+        public string Pw { get; set; }
+    }
+}
---
+++ Envir/Envir/Services/UserService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Envir.Models;
+using MongoDB.Driver;
+
+namespace Envir.Services
+{
+    public class UserService
+    {
+        private readonly IMongoCollection<User> _users;
+
+        public UserService(IConfiguration config)
+        {
+            var client = new MongoClient(config.GetConnectionString("UserCS"));
+            var database = client.GetDatabase("Skynet");
+
+            _users = database.GetCollection<User>("SkynetData");
+        }
+        /// Get method for alle brugere
+        public List<User> Get() => _users.Find(user => true).ToList();
+
+        public User Get(string id) => _users.Find<User>(user => user.Id == id).FirstOrDefault();
+
+        public User Create(User user)
+        {
+            _users.InsertOne(user);
+            return user;
+        }
+
+        public void Update(string id, User userUp) => _users.ReplaceOne(user => user.Id == id, userUp);
+
+        public void Remove(User userUp) => _users.DeleteOne(user => user.Id == userUp.Id);
+    }
+}
