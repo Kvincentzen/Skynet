4b16c66f2144894aee9381d9750924757bdb9815
Jakob Vestergaard <jakobmetalv@gmail.com>
2020-08-13 14:38:16 +0200

    Routing added

--- Angular/ClientApp/src/app/app.module.ts
+++ Angular/ClientApp/src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { Test1Component } from './test1/test1.component';
 import { Test2Component } from './test2/test2.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     Test1Component,
-    Test2Component
+    Test2Component,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
